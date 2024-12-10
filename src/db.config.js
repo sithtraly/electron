2fc7f3f3daf09b6aca51d67488efef2360d49b58
Traly Sith <sithtraly147@gmail.com>
2024-12-10 18:11:01 +0700

    create table and make relationship

--- src/db.config.js
+++ src/db.config.js
@@ -17,14 +17,37 @@ const CustomerModel = sequelize.define('tb_customer', {
   modelName: 'tb_customer'
 })
 
+const ProductModel = sequelize.define('tb_product', {
+  name: DataTypes.STRING
+}, {
+  timestamps: true,
+  freezeTableName: true,
+  modelName: 'tb_product'
+})
+
+const OrderModel = sequelize.define('tb_order', {
+  qty: DataTypes.NUMBER,
+  price: DataTypes.NUMBER
+}, {
+  timestamps: true,
+  freezeTableName: true,
+  modelName: 'tb_order'
+})
+
+CustomerModel.hasMany(OrderModel, { foreignKey: 'customerId' })
+OrderModel.belongsTo(CustomerModel, { foreignKey: 'customerId' })
+
+ProductModel.hasMany(OrderModel, { foreignKey: 'productId' })
+OrderModel.belongsTo(ProductModel, { foreignKey: 'productId' })
+
 async function connectdb() {
   try {
     await sequelize.authenticate()
-    await sequelize.sync({alter: true})
+    await sequelize.sync({ alter: true })
     console.log("Conntected to database successfully")
   } catch (err) {
     console.error("Cannot conntect to database", err)
   }
 }
 
-module.exports = { sequelize, connectdb, CustomerModel }
+module.exports = { sequelize, connectdb, CustomerModel, ProductModel, OrderModel }
