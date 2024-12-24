448fc12c3555c3f3cc55f01ba6b452b16fc5e88a
Traly Sith <sithtraly147@gmail.com>
2024-12-24 18:17:56 +0700

    Add checkbox to order

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,9 +1,10 @@
 app.controller('OrderController', ['$scope', '$location', 'ShareData', function ($scope, $location, ShareData) {
   $scope.orders = []
+  $scope.allSelected = false;
   $scope.getOrder = function () {
     window.api.getOrder().then(function (orders) {
       if (orders) $scope.$apply(function () {
-        orders.map(function(o) {o.createdAt = DateUtil.datetime2stdDatetime(o.createdAt)})
+        orders.map(function (o) { o.createdAt = DateUtil.datetime2stdDatetime(o.createdAt) })
         $scope.orders = orders
       })
     })
@@ -16,8 +17,30 @@ app.controller('OrderController', ['$scope', '$location', 'ShareData', function 
   $scope.newOrder = function () {
     $location.path('/newOrder')
   }
-  $scope.dblClick = function(order) {
+  $scope.dblClick = function (order) {
     ShareData.set('order', order)
     $location.path('/newOrder')
+  }
+
+  $scope.toggleAll = function () {
+    let status = $scope.allSelected
+    angular.forEach($scope.orders, function (o) {
+      o.selected = status
+    })
+  }
+  $scope.toggleSelect = function () {
+    let total = $scope.orders.length
+    let selected = $scope.orders.filter(function (o) {
+      return o.selected
+    })
+    document.getElementById('selectAll').indeterminate = false
+    if (selected.length === 0) {
+      $scope.allSelected = false
+    } else if (selected.length === total) {
+      $scope.allSelected = true
+    } else {
+      $scope.allSelected = undefined
+      document.getElementById('selectAll').indeterminate = true
+    }
   }
 }])
